94248c2008d845b031d6d220618d24b6eaedc1ab
Onelevenvy <qingxin1114@163.com>
2024-10-21 09:35:28 +0800

    chore:remove unused import

--- web/src/app/(applayout)/dashboard/page.tsx
+++ web/src/app/(applayout)/dashboard/page.tsx
@@ -118,8 +118,7 @@ function Dashboard() {
     TeamsService.readTeams({})
   );
 
-  const bgColor = useColorModeValue("white", "gray.800");
-  const borderColor = useColorModeValue("gray.200", "gray.700");
+
 
   return (
     <Container maxW="full" p={0}>
