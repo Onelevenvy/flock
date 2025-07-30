557bc0077619ed44bdfa443c979291f43fe3929a
Onelenvenvy <qingxin1114@163.com>
2025-07-30 10:01:25 +0800

    feat: add load_dotenv method in main.py to resolve issues with certain compilers failing to initialize the environment variables through the .env file

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,11 @@
+from dotenv import load_dotenv
+from pathlib import Path
+
+env_path = Path(__file__).resolve().parent.parent.parent / ".env"
+load_dotenv(dotenv_path=env_path)
+
+
+
 from contextlib import asynccontextmanager
 
 import sentry_sdk
